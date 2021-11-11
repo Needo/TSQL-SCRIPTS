d972c252253f4b1d4d04e0d0347c45889c9ea0ce
Munawar Hussain <needo@msn.com>
2021-11-11 18:09:49 +0500

    Update Restore_databases_bakup_in_batch_unattended.sql
    Added first step which is a prerequisite to the 2nd step

--- Restore_databases_bakup_in_batch_unattended.sql
+++ Restore_databases_bakup_in_batch_unattended.sql
@@ -1,3 +1,21 @@
+Step-1: Enable / Configure extebded SP.
+
+-- To allow advanced options to be changed.
+EXEC sp_configure 'show advanced options', 1;
+GO
+-- To update the currently configured value for advanced options.
+RECONFIGURE;
+GO
+-- To enable the feature.
+EXEC sp_configure 'xp_cmdshell', 1;
+GO
+-- To update the currently configured value for this feature.
+RECONFIGURE;
+GO
+
+*******************************************************
+
+Step-2
 
 DECLARE @BacukupsSourceFolder nvarchar(100) ='C:\Users\DemoUser\Documents\Databases\'
 DECLARE @RestoreToDestinationFolder varchar(1000)='C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\'
@@ -24,9 +42,14 @@ DECLARE @FileFullNameWithOutExt varchar(1000) = SUBSTRING(@FilesCmdshellOutputCm
 SET @SearchedCharIndex=CHARINDEX('_', REVERSE(N''+ @FileFullNameWithOutExt ))-1
 SET @DbNameWithoutDatePostFix=SUBSTRING(N''+ @FileFullNameWithOutExt,0,LEN(N''+ @FileFullNameWithOutExt)- @SearchedCharIndex)
 
+--***WARNING: @DbNameWithoutDatePostFix is only required in case you need to remove some postfix value from file name which is usually added due to naming a backup file while its not part of database name.
+
 DECLARE @sqlRestore NVARCHAR(MAX) = 'RESTORE DATABASE [' + @FileFullNameWithOutExt +'] FROM DISK = N'''+ @BacukupsSourceFolder + @FileFullNameWithOutExt + '.bak'' WITH FILE = 1, MOVE N''' + @DbNameWithoutDatePostFix + ''' TO N'''+@RestoreToDestinationFolder + @FileFullNameWithOutExt + '.mdf'', MOVE N''' + @FileFullNameWithOutExt + '_log'' TO N'''+@RestoreToDestinationFolder  + @FileFullNameWithOutExt + '_log.ldf'', NOUNLOAD, STATS = 10'
 --Print(@sqlRestore)
 EXEC(@sqlRestore)
 
 FETCH NEXT FROM @FilesCmdshellCursor INTO @FilesCmdshellOutputCmd
 END
+
+
+**********************
